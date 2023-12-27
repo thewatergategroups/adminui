03f8d24e987a26b7dcac7af6dd28117ddc70c6a4
ciaran mckey <ciaranmckey@gmail.com>
2023-12-27 02:36:32 +0000

    make initial page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
+import { useState } from 'react';
 import './App.css'
-import LoginPage from './components/LoginPage'
+import SignIn  from './components/SignIn'
+import Dashboard  from './components/Dashboard'
+import Cookies from 'js-cookie';
+
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    // Function to handle successful login
+    const handleLogin = () => {
+      setIsLoggedIn(true);
+    };
+  
+    const handleLogout = () => {
+      setIsLoggedIn(false);
+      Cookies.remove("Authorization")
+    };
 
   return (
     <>
-      <LoginPage/>
+      {isLoggedIn ? (
+        // Show DashboardPage if logged in
+        <Dashboard onLogout={handleLogout} />
+      ) : (
+        // Show LoginPage if not logged in
+        <SignIn onLogin={handleLogin} />
+      )}
     </>
   )
 }
---
+++ src/components/Dashboard.tsx
@@ -0,0 +1,22 @@
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import React from 'react';
+
+interface DashboardProps {
+  onLogout: () => void; 
+}
+
+const Dashboard: React.FC<DashboardProps> = ({ onLogout }) =>{
+  return (
+    <div>
+      <Typography variant="h4" gutterBottom color="black">
+        Welcome
+      </Typography>
+      <Button variant="contained" color="primary" onClick={onLogout}>
+        Logout
+      </Button>
+    </div>
+  );
+};
+
+export default Dashboard
