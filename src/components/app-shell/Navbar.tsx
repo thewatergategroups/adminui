f646f24f844fa9f4bc9f31027e2bc1e1c452e959
davidculemann <davidculemann@gmail.com>
2024-04-28 10:53:10 +0100

    improve navbar

--- src/components/app-shell/Navbar.tsx
+++ src/components/app-shell/Navbar.tsx
@@ -1,12 +1,29 @@
 import { NavLink } from "@mantine/core";
-import { IconLayoutDashboard, IconUsers } from "@tabler/icons-react";
+import { IconLayoutDashboard, IconSettings, IconUsers } from "@tabler/icons-react";
+import { useLocation } from "react-router-dom";
+import styled from "styled-components";
+
+const StyledNavbar = styled.nav`
+  display: flex;
+  flex-direction: column;
+  gap: 0.6rem;
+`;
 
 export default function Navbar() {
+  const location = useLocation();
+  const isActiveTab = (href: string) => location.pathname === href;
+
+  const navItems = [
+    { href: "/", label: "Dashboard", icon: <IconLayoutDashboard size="1rem" stroke={1.5} />, active: isActiveTab("/") },
+    { href: "/users", label: "Users", icon: <IconUsers size="1rem" stroke={1.5} />, active: isActiveTab("/users") },
+    { href: "/settings", label: "Settings", icon: <IconSettings size="1rem" stroke={1.5} />, active: isActiveTab("/settings") },
+  ];
+
   return (
-    <div>
-      Navbar
-      <NavLink href="/users" label="Users" leftSection={<IconUsers size="1rem" stroke={1.5} />} />
-      <NavLink href="/" label="Dashboard" leftSection={<IconLayoutDashboard size="1rem" stroke={1.5} />} />
-    </div>
+    <StyledNavbar>
+      {navItems.map((item) => (
+        <NavLink key={item.href} href={item.href} label={item.label} leftSection={item.icon} active={item.active} />
+      ))}
+    </StyledNavbar>
   );
 }
